973f3752c72a701cd9b070984fe68471a2e7b8d8
Your Name <you@example.com>
2018-07-19 17:12:21 +0800

    Add component navbar

--- test-mvc-angular/_fiona_readme.txt
+++ test-mvc-angular/_fiona_readme.txt
@@ -4,3 +4,14 @@
 - .NET Core 2.1 + API 
 - Refer to this tutorial: http://www.talkingdotnet.com/how-to-create-an-angular-6-app-with-visual-studio-2017/
 
+——————————————————————————————————————————————————————
+Updates:
+==========================
+
+#Add component [navbar]
+@CMD ==> C:\projects\test-mvc-angular\test-mvc-angular
+>ng generate component navbar
+
+==========================
+
+
--- test-mvc-angular/wwwroot/styles.js
+++ test-mvc-angular/wwwroot/styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! c:\users\fionalim\source\repos\test-mvc-angular\test-mvc-angular\src\styles.scss */"./src/styles.scss");
+module.exports = __webpack_require__(/*! C:\Users\fionalim\source\repos\test-mvc-angular\test-mvc-angular\src\styles.scss */"./src/styles.scss");
 
 
 /***/ })
