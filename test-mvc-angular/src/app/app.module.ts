f3fe9fcc94c9b25a7fffb34e2c8a96ba16d925c9
Your Name <you@example.com>
2018-07-20 11:23:41 +0800

    Create ng component 'Product'

--- test-mvc-angular/src/app/app.module.ts
+++ test-mvc-angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
